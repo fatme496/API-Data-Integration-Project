bddb824f6d5d12cc8fd1f81565e490b0d6efc80e
Fatme <fatme34saeed@gmail.com>
2025-01-08 23:36:04 +0200

    Detailed-Information-Page

--- Script.js
+++ Script.js
@@ -11,8 +11,9 @@ const fetchesAndDisplay = async () => {
       postElement.innerHTML = `
       <div class="grid-container">
       <div class="grid-item">
-        <div><a href="${post.thumbnailUrl}"><img src="${post.url}"></a></div>
+        <div class="img-container"><a href="${post.thumbnailUrl}"><img src="${post.url}"></a></div>
         <h2>${post.title}</h2>
+        <button onclick="window.location.href='Detailed-Information-Page.html?photoId=${post.id}';">See More Details</button>
       </div>
       </div>`;
       postsContainer.appendChild(postElement);
@@ -23,3 +24,24 @@ const fetchesAndDisplay = async () => {
 };
 
 fetchesAndDisplay();
+
+const apiUrl = "https://jsonplaceholder.typicode.com/photos";
+
+const fetchAndDisplay = async () => {
+  try {
+    const response2 = await fetch(apiUrl);
+    const photo = await response2.json();
+
+    const Container = document.getElementById("photo-card");
+    photo.forEach(ph => {
+      const photoElement = document.createElement("div");
+      photoElement.innerHTML = `
+      <img src="${ph.url}" alt="Album Photo" class="photo">
+      <h2 class="album-title">Album Title</h2>
+      <h3>https://jsonplaceholder.typicode.com/albums/${ph.albumId}/title</h3>`;
+      Container.appendChild(photoElement);
+    });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+};
